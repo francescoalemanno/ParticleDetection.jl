a335c07ca0d6e1f66a83fd3d8f290edc56dbf0b2
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-03-10 00:23:15 +0100

    ugly but fun

--- src/ParticleDetection.jl
+++ src/ParticleDetection.jl
@@ -8,6 +8,23 @@ module ParticleDetection
         m/length(Is)
     end
 
+    struct op_gaussian <: Function
+        sn::Int
+    end
+    @inline function (p::op_gaussian)(A::AbstractArray{T}, Is, I) where T
+        ws=zero(T)
+        s=ws
+        K1=convert(T,pow2(p.sn)::Int)::T
+        @inbounds for i in Is
+            deltaX=convert.(T,Tuple(i-I))
+            delta=sum(pow2,deltaX)::T/K1::T
+            w=exp(-T(2)*delta::T)
+            s+=A[i]*w
+            ws+=w
+        end
+        s/ws
+    end
+
     @inline function op_max(A,Is,I)
         m=A[I]
         @inbounds @simd for i in Is
@@ -20,37 +37,20 @@ module ParticleDetection
         x*x
     end
     export bp_filter
+
     function bp_filter(image,sn,so)
         fobject=KernelOp(op_mean,image,(so,so))
-        fnoise=KernelOp(image,(sn,sn)) do A, Is, I
-            ws=zero(eltype(A))
-            s=ws
-            @inbounds for i in Is
-                deltaX=Tuple(i-I)
-                delta=sum(pow2,deltaX)/pow2(sn)
-                w=exp(-2delta)
-                s+=A[i]*w
-                ws+=w
-            end
-            s/ws
-        end
-        bpass=KernelOp(collect(fnoise),(0,0)) do A,Is,I
-            @inbounds max(A[I]-fobject[I],zero(eltype(image)))
-        end
-        bpass
+        fnoise=KernelOp(op_gaussian(sn),image,(sn,sn))
+        Z=zero(eltype(image))
+        @. max(fnoise-fobject,Z)
     end
     export local_maxima
     function local_maxima(image,abs_th=zero(eltype(image)))
         abs_th=convert(eltype(image),abs_th)
         ONE=one(eltype(image))
         ZERO=zero(eltype(image))
-        KernelOp(collect(image),(1,1)) do A,Is,I
-            @inbounds om=m=A[I]
-            @inbounds @simd for i in Is
-                m=max(m,A[i])
-            end
-            (m==om && m>abs_th) ? ONE : ZERO
-        end
+        max_im=KernelOp(op_max,image,(1,1))
+        (max_im .== image) .& (image .> abs_th)
     end
 
 
