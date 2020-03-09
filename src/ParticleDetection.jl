d1b61d4a6f9ca58e1af78037266d2dcccd08533d
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-03-09 23:32:05 +0100

    drafted

--- src/ParticleDetection.jl
+++ src/ParticleDetection.jl
@@ -1,78 +1,59 @@
 module ParticleDetection
+    using KernelOps
+    @inline function op_mean(A,Is,I)
+        m=zero(eltype(A))
+        @inbounds @simd for i in Is
+            m+=A[i]
+        end
+        m/length(Is)
+    end
+
+    @inline function op_max(A,Is,I)
+        m=A[I]
+        @inbounds @simd for i in Is
+            m=max(m,A[i])
+        end
+        m
+    end
+
+    @inline function pow2(x)
+        x*x
+    end
+    export bp_filter
+    function bp_filter(image,sn,so)
+        fobject=KernelOp(op_mean,image,(so,so))
+        fnoise=KernelOp(image,(sn,sn)) do A, Is, I
+            ws=zero(eltype(A))
+            s=ws
+            @inbounds for i in Is
+                deltaX=Tuple(i-I)
+                delta=sum(pow2,deltaX)/pow2(sn)
+                w=exp(-2delta)
+                s+=A[i]*w
+                ws+=w
+            end
+            s/ws
+        end
+        bpass=KernelOp(collect(fnoise),(0,0)) do A,Is,I
+            @inbounds max(A[I]-fobject[I],zero(eltype(image)))
+        end
+        bpass
+    end
+    export local_maxima
+    function local_maxima(image,abs_th=zero(eltype(image)))
+        abs_th=convert(eltype(image),abs_th)
+        ONE=one(eltype(image))
+        ZERO=zero(eltype(image))
+        KernelOp(collect(image),(1,1)) do A,Is,I
+            @inbounds om=m=A[I]
+            @inbounds @simd for i in Is
+                m=max(m,A[i])
+            end
+            (m==om && m>abs_th) ? ONE : ZERO
+        end
+    end
+
 
 end # module
 
 #%%
-
-
-struct KernelOp{T,N,S} <: AbstractArray{T,N}
-    parent::S
-    op::Function
-    opdims::CartesianIndex{N}
-    lowbound::CartesianIndex{N}
-    upbound::CartesianIndex{N}
-    @inline function KernelOp{T,N,S}(A::S,operation::Function,dims::CartesianIndex{N}) where {T,N,S <: AbstractArray{T,N}}
-        l=minimum(CartesianIndices(A))
-        h=maximum(CartesianIndices(A))
-        new(A,operation,dims,l,h)
-    end
-end
-@inline function KernelOp(A::AbstractArray{T,N},op::Function,dims::NTuple{N,Int}) where {T,N}
-    KernelOp{eltype(A),ndims(A),typeof(A)}(A,op,CartesianIndex(dims))
-end
-import Base.size, Base.getindex, Base.IndexStyle
-
-@inline IndexStyle(::KernelOp) = IndexCartesian()
-@inline function size_parent(A::KernelOp)
-    size(A.parent)
-end
-@inline function size(A::KernelOp)
-    return size_parent(A)
-end
-@inline function getindex(A::KernelOp{T,N,S},Index::Vararg{Int,N}) where {T,N,S}
-    I=CartesianIndex(Index)
-    @boundscheck checkbounds(A,I)
-    @inbounds begin
-        lowI=max(I-A.opdims,A.lowbound)
-        upI=min(I+A.opdims,A.upbound)
-        return A.op(A.parent,lowI:upI,I)
-    end
-end
-
-@inline function op_mean(A,r,c)
-    m=zero(eltype(A))
-    @inbounds begin
-        @simd for i in r
-            m+=A[i]
-        end
-    end
-    m/length(r)
-end
-@inline function op_max(A,r,c)
-    m=A[r[1]]
-    @inbounds begin
-        @simd for i in r
-            m=max(m,A[i])
-        end
-    end
-    m
-end
-
-
-M=rand(1000,1000)
-f1=KernelOp(M,op_mean,(1,1))
-f2=KernelOp(f1,op_max,(2,2))
-
-@time maximum(f2)
-
-
-using Serialization
-
-using PyPlot
-
-ima=deserialize("test/particles.bin")
-
-pygui(true)
-
-imshow(ima)
-scatter(1:10,1:10)
