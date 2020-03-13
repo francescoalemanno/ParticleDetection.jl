032f73db69938bd75c053f2211d2eab8759d5487
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-03-13 14:45:52 +0000

    build based on aa12852

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"index.html#ParticleDetection-1","page":"ParticleDetection","title":"ParticleDetection","text":"","category":"section"},{"location":"index.html#","page":"ParticleDetection","title":"ParticleDetection","text":"Documentation goes here.","category":"page"}]
+[{"location":"index.html#ParticleDetection-1","page":"ParticleDetection","title":"ParticleDetection","text":"","category":"section"},{"location":"index.html#","page":"ParticleDetection","title":"ParticleDetection","text":"","category":"page"},{"location":"index.html#","page":"ParticleDetection","title":"ParticleDetection","text":"Modules = [AdaMeanShift]","category":"page"}]
 }
